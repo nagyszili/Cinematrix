4ded9b27af70a99ccf6571d588cb05192a183611
Nagy Szilard <n_szili98@yahoo.com>
2019-12-12 14:35:21 +0200

    NowInCinemaFragment, Connect to DB

---
+++ app/src/main/java/com/example/cinematrix/NowInCinemaFragment.java
@@ -0,0 +1,22 @@
+package com.example.cinematrix;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class NowInCinemaFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_now_in_cinema, container, false);
+
+        return view;
+    }
+}
